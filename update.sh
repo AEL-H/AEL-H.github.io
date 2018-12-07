642436a8a563c8e7f1c4cc337dac64ca670eb6b2
AEL-H <elhamdy.amir@yahoo.co.uk>
2018-12-07 16:25:13 +0000

    Delete update.sh

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# ENSURE VPN IS ON
-
-sudo mount -t cifs -o user=mbcx4ae3,domain=ds.man.ac.uk,sec=ntlmsspi //nask.man.ac.uk/home$ /mnt/p_drive
-sudo rsync -rtvu --delete . /mnt/p_drive/public_html/
-# sudo cp -v -r -u . /mnt/p_drive/public_html/
-sudo umount /mnt/p_drive
